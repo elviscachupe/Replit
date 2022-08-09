34c8cfdd7e990354f2b2f0cfee1bcb20bd89b494
elviscachupe <109166927+elviscachupe@users.noreply.github.com>
2022-08-09 13:35:24 -0600

    Create Valor absoluto.py

---
+++ Valor absoluto.py
@@ -0,0 +1,11 @@
+#Este programa recibe un numero y muestra el valor absoluto de este
+
+
+n1=float(input("Ingrese un numero entero"))
+if n1 >= 0:
+  res=n1*1
+  print("El valor absoluto es: ",res)
+else:
+  n1 <= -1
+  res=n1*-1
+  print("El valor absoluto es: ",res)

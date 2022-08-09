18ba44a7fed1acee257087a9f8f7d76a335590cf
elviscachupe <109166927+elviscachupe@users.noreply.github.com>
2022-08-09 13:31:30 -0600

    Create Menor o igual.py

---
+++ Menor o igual.py
@@ -0,0 +1,14 @@
+#Este programa identifica si los dos numeros ingresados son iguales y también identifica cual de los dos es menor
+
+
+n1=float(input("Ingrese primer numero"))
+n2=float(input("ingrese segundo numero"))
+if n1 < n2:
+  print(n1,"este numero es menor")
+  
+elif n1 <= n2: 
+  print(n1,"estos numeros son inguales")
+
+else:
+  n2 < n1
+  print(n2,"este numero es menor")

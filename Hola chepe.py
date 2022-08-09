9d512625259d2af23aae60e29768353618c5d5e4
elviscachupe <109166927+elviscachupe@users.noreply.github.com>
2022-08-09 13:11:01 -0600

    Create Hola chepe.py

---
+++ Hola chepe.py
@@ -0,0 +1,33 @@
+#Este problema realiza las operaciones básicas y dice "Hola chepe"
+
+print("Hola chepe")
+
+print("suma de dos numeros")
+n1=5
+n2=2 
+res=n1+n2
+print(res)
+
+print("resta de dos numeros")
+n1=10
+n2=8
+res=n1-n2
+print(res)
+
+print("multiplicacion de dos numeros")
+n1=6
+n2=2
+res=n1*n2
+print(res)
+
+print("division de dos numeros")
+n1=8
+n2=2
+res=n1/n2
+print(res)
+
+print("modulo de dos numeros")
+n1=100
+n2=500
+res=n1%n2
+print(res)

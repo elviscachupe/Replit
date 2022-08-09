8ca802c6770f9b2e77b12e375312ca3adfb02a62
elviscachupe <109166927+elviscachupe@users.noreply.github.com>
2022-08-09 13:55:48 -0600

    Create Programa que cuenta caracteres.py

---
+++ Programa que cuenta caracteres.py
@@ -0,0 +1,7 @@
+#Este programa cuenta los caracteres de una frase
+
+
+n1=str(input("Ingrese una frase: "))
+n2=str(input("Ingrese caracter para contar: "))
+n3=n1.count(n2)
+print("Se repite ",n3, "veces")

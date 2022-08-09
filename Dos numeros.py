5e59f66624ead59dadcdeeb352c815daf49b80f9
elviscachupe <109166927+elviscachupe@users.noreply.github.com>
2022-08-09 13:30:16 -0600

    Create Dos numeros.py

---
+++ Dos numeros.py
@@ -0,0 +1,10 @@
+#Este programa recibe dos numeros e identifica cual es el numero menor
+
+
+n1=float(input("Ingrese primer numero"))
+n2=float(input("ingrese segundo numero"))
+if n1 < n2 :
+  print(n1,"este numero es menor")
+else:
+  n2 < n1
+  print(n2,"este numero es menor")

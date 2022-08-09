8851fee82e8cad04134403b7990282a93557cc9e
elviscachupe <109166927+elviscachupe@users.noreply.github.com>
2022-08-09 13:17:58 -0600

    Create Numero par o impar.py

---
+++ Numero par o impar.py
@@ -0,0 +1,8 @@
+#Este programa identifica cuando un numero es par o impar
+
+
+n1=int(input("Ingresa un numero para comprobar si es par o impar"))
+if n1 % 2 == 0:
+  print("el numero es par")
+else:
+  print("el numero es impar")

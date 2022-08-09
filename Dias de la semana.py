ce857920905dadec3355a070162cb5c6a8e7fc29
elviscachupe <109166927+elviscachupe@users.noreply.github.com>
2022-08-09 13:33:04 -0600

    Create Dias de la semana.py

---
+++ Dias de la semana.py
@@ -0,0 +1,38 @@
+#Este programa recibe un numero que segun el día de la semana que sea manda un mensaje para la persona
+
+
+print("1.Lunes")
+print("2.Martes")
+print("3.Miercoles")
+print("4.Jueves")
+print("5.Viernes")
+print("6.Sabado")
+print("7.Domingo")
+n1=float(input("Ingrese un numero segun el dia"))
+if n1 == 1:
+  print("Lunes")
+  print("Feliz inicio de semana")
+  
+elif n1 == 2:
+  print("Martes")
+  print("Espero te vaya muy buien en tu semana")
+
+elif n1 == 3:
+  print("Miercoles")
+  print("Espero te vaya muy buien en tu semana")
+
+elif n1 == 4:
+  print("Jueves")
+  print("Espero te vaya muy buien en tu semana")
+
+elif n1 == 5:
+  print("Viernes")
+  print("Feliz fin de semana")
+
+elif n1 == 6:
+  print("Sabado")
+  print("Que disfrutes tu sabado")
+
+elif n1 == 7:
+  print("Domingo")
+  print("Tomate un descanso")

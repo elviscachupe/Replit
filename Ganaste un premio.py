5c669f9401e7afc0e9a142aa06f43be083ae6145
elviscachupe <109166927+elviscachupe@users.noreply.github.com>
2022-08-09 13:29:03 -0600

    Create Ganaste un premio.py

---
+++ Ganaste un premio.py
@@ -0,0 +1,8 @@
+#Este programa recibe un numero y si el numero coincide con el numero del ganador del premio es la persona ganadora
+
+
+n1=float(input("Ingrese numero de cliente"))
+if n1 == 1000:
+  print("Ganaste un premio")
+else:
+  print("Gracias por participar")

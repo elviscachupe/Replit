317399bf991cd42026e20d317556c25a118fe28d
elviscachupe <109166927+elviscachupe@users.noreply.github.com>
2022-08-09 13:52:49 -0600

    Create mitad de diamante.py

---
+++ mitad de diamante.py
@@ -0,0 +1,10 @@
+#Este es el ejemplo de un programa que muestra la mitad del diamante
+
+
+print("Programa de asteriscos")
+
+i=10
+for n in range ( 0, 10):
+  for i in range ( 0, i):
+    print("* ", end="")
+  print()

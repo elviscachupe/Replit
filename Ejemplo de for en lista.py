e2ce54141a52fcc0c7f6afd37adf76d6952ebef2
elviscachupe <109166927+elviscachupe@users.noreply.github.com>
2022-08-09 13:48:01 -0600

    Create Ejemplo de for en lista.py

---
+++ Ejemplo de for en lista.py
@@ -0,0 +1,12 @@
+#Este es un ejemplo de For en lista 
+
+
+frutas = ["Sandia", "Banano", "Manzana","Naranja"]
+for fruta in frutas:
+  print(fruta)
+
+for l in "Gabriel":
+  print(l)
+
+for x in range(-100,0,2):
+  print(x)

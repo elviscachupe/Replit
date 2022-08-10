f97862f58d854e308a578bb27d71271bbf484e98
elviscachupe <109166927+elviscachupe@users.noreply.github.com>
2022-08-09 21:16:36 -0600

    Create Raiz.py

---
+++ Raiz.py
@@ -0,0 +1,9 @@
+#El funcionamiento de este programa es que vamos sacar la raiz de nuestro numero n
+
+
+n=float(input("Ingrese un numero: "))
+r=float(input("Ingrese una raiz: "))
+
+f=n**(1/r)
+
+print(f)

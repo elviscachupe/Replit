f2b8509dff828e6d84541c9f5a1a26fed2a40ba1
elviscachupe <109166927+elviscachupe@users.noreply.github.com>
2022-08-09 21:09:08 -0600

    Create 10 numeros enteros.py

---
+++ 10 numeros enteros.py
@@ -0,0 +1,19 @@
+#Este programa recibe 10 numeros, suma el primero y el ultimo, y luego los suma todos
+
+
+n1=float(input("Ingrese primer numero: "))
+n2=float(input("Ingrese primer numero: "))
+n3=float(input("Ingrese primer numero: "))
+n4=float(input("Ingrese primer numero: "))
+n5=float(input("Ingrese primer numero: "))
+n6=float(input("Ingrese primer numero: "))
+n7=float(input("Ingrese primer numero: "))
+n8=float(input("Ingrese primer numero: "))
+n9=float(input("Ingrese primer numero: "))
+n10=float(input("Ingrese primer numero: "))
+
+x=n1+n10
+print("El resultado es: ",x)
+
+y=n2+n3+n4+n5+n6+n7+n8+n9
+print("El resultado es: ",y)

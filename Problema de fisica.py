cf2738492d6135c7752429f2b144a6886a601d46
elviscachupe <109166927+elviscachupe@users.noreply.github.com>
2022-08-09 21:02:10 -0600

    Create Problema de fisica.py

---
+++ Problema de fisica.py
@@ -0,0 +1,8 @@
+#Este programa pide una velocidad y un tiempo y a cambío nos da la distancia que recorrio un movil
+
+
+a=int(input("Ingrese la velocidad: "))
+b=int(input("Ingrese el tiempo: "))
+
+x=a*b
+print("La distancia es: ",x)

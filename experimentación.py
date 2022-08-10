e21e3da4a629ade69aaba9ad311c0843f7c4985f
elviscachupe <109166927+elviscachupe@users.noreply.github.com>
2022-08-09 21:00:57 -0600

    Create experimentación.py

---
+++ experimentación.py
@@ -0,0 +1,43 @@
+#Este programa pide un nombre de usuario y contraseña e identifica si los ultimos 3 dijitos del nombre de usuario son numeros
+
+
+n1=str(input("Ingrese nombre de usuario: "))
+n2=str(input("Ingrese contraseña: "))
+a="0"
+b="1"
+c="2"
+d="3"
+e="4"
+f="5"
+g="6"
+h="7"
+i="8"
+j="9"
+
+
+x= n1[-1]
+z= n1[-2]
+p= n1[-3]
+ 
+if x == a :
+  print("Nombre de usuario con numeros")
+elif x == b :
+  print("Nombre de usuario con numeros")
+elif x == c :
+  print("Nombre de usuario con numeros")
+elif x == d :
+  print("Nombre de usuario con numeros")
+elif x == e :
+  print("Nombre de usuario con numeros")
+elif x == f :
+  print("Nombre de usuario con numeros")
+elif x == g :
+  print("Nombre de usuario con numeros")
+elif x == h :
+  print("Nombre de usuario con numeros")
+elif x == i :
+  print("Nombre de usuario con numeros")
+elif x == j :
+  print("Nombre de usuario con numeros")
+else:
+  print("Los ultimos 3 caracteres deben ser numeros")
